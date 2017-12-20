92ef9e465bda67b694848f92e910e8d3784b51c9
lspavan <lakshmisetty@gmail.com>
2017-12-20 13:05:09 +0100

    Registration modifictaion with respect to legistlation

--- src/g/CreateServlet.java
+++ src/g/CreateServlet.java
@@ -26,31 +26,41 @@ public class CreateServlet extends HttpServlet {
 		PrintWriter out=response.getWriter();
 		int accn=0;
 		int status=-1;
-		String username=request.getParameter("username");
+		String personalnumber=request.getParameter("personalnumber");
 		String fullname=request.getParameter("fullname");
-		String password=request.getParameter("password");
-		String  repassword=request.getParameter("repassword");
-		
-		String amoun=request.getParameter("amount");
-		double amount=Double.parseDouble(amoun);
-		
+	
 		String adderess=request.getParameter("adderess");
 		
 		String ph=request.getParameter("phone");
 		String email=request.getParameter("email");
-		System.out.println("Email id is :::"+email);
-		double phone=Double.parseDouble(ph);
 		
-		if(email.contains("@") && (email.contains(".com") || email.contains(".in") || email.contains(".se") )){
+		System.out.println("Email id is :::"+email);
+		System.out.println("Personal number and full name from Page "+personalnumber+"--"+fullname);
+		double phone=Double.parseDouble(ph);
+		//double mname=Double.parseDouble(num);
+		//String country=request.getParameter("country");
+		
+		
+		//Validating the with the personal number database
+				PersonalNumberService pns=new PersonalNumberService();
+				boolean check=pns.validate(personalnumber.trim(),fullname.trim());
+		
+		//Encrypt Personal Number
+				SecurityG3 s1=new SecurityG3();
+		String perNo=s1.encrypt(personalnumber,"ssshhhhhhhhhhh!!!!");
+		
+		System.out.println("encrypted personal nubmer is:: "+ perNo);
+		
+		if(email.contains("@") && (email.contains(".com") || email.contains(".in") || email.contains(".se") && check ) && check){
 			System.out.println("email contaiing @");
-	     status=RegisterUser.register(username,fullname, password, repassword, amount, adderess,phone,email);
+	     status=RegisterUser.register(perNo,fullname,adderess,phone,email,"create");
 		}
 	    
 
 try{
 		    Connection con1=GetCon.getCon();		
 			PreparedStatement ps1=con1.prepareStatement("Select max(accountno) from newaccount");
-		
+			//PreparedStatement ps1=con1.prepareStatement("Select accountno from NEWACCOUNT where accountno='"+passedaccountno+"'");
             ResultSet rs1=ps1.executeQuery();
             while(rs1.next()){
 				int  accountno=rs1.getInt(1);
@@ -60,12 +70,15 @@ try{
 			Connection con=GetCon.getCon();
 			PreparedStatement ps=con.prepareStatement("Select * from NEWACCOUNT where accountno='"+accountno+"'");
 			System.out.println("Interger converted one:::"+accountno);
-        
+           // ps.setInt(1,accountno);
 			ResultSet rs=ps.executeQuery();
 			
+			/*out.print("<table align='left'  cellspacing='5' cellpadding='5'>");
+			out.print("<tr><th>ACCOUNT NO</th><th>USERNAME</th><th>PASSWORD</th><th>AMOUNT</th><th>ADDRESS</th><th>PHONE</th></tr>");*/
 			while(rs.next()){				
 				
 				 accn=rs.getInt(1);
+				//String email=rs.getString(8);				    
 			}
 			
 }catch(Exception e){
@@ -73,17 +86,17 @@ try{
 }
 	    
 		if(status>0){
-		
+			//out.print("WELCOME! YOUR ACCOUNT HAS OPENED");
 			Main m=new Main();
 			Integer accno = Integer.valueOf(accn);
-	    	m.sendFromGMail("Welcome to HEM Bank","Your account number is  "+accno+"\n"+"Please use the below link to login to the application"+"\n"+"http://localhost:8086/HEMBANK/"+"\n"+ "\n"+"Thanks,"+"\n"+"HEM Bank",email);
+	    	m.sendFromGMail("Welcome to HEM Bank","Your account number is  "+accno+"\n"+"Please use the below link to activate your account"+"\n"+"http://localhost:8086/HEMBANK/"+"\n"+ "\n"+"Thanks,"+"\n"+"HEM Bank",email);
 	    	
 			
 			RequestDispatcher rd=request.getRequestDispatcher("successregistration.jsp");
 			rd.include(request, response);
 		}
 		else{
-			out.print("Sorry,Registration failed. please try later");
+			//out.print("Sorry,Registration failed. please try later");
 			RequestDispatcher rd=request.getRequestDispatcher("MyHtml.html");
 			rd.include(request, response);
 		}
--- src/g/RegisterUser.java
+++ src/g/RegisterUser.java
@@ -5,24 +5,25 @@ import g.GetCon;
 public class RegisterUser {
 static int status=0;
 //int accountno=1;
-public static int register(String username,String fullname,String password,String repassword,double amount,String adderess,double phone,String email){
-	//public static int register(String email,String password,String gender,String country,String name){
+public static int register(String personalnumber,String fullname,String address,double phone,String email,String statu){
+	
 
 	Connection con=GetCon.getCon();
 	PreparedStatement ps;
 	try {
-		ps = con.prepareStatement("Insert into NEWACCOUNT values(?,?,?,?,?,?,?,?,?)");
+		ps = con.prepareStatement("Insert into NEWACCOUNT values(?,?,?,?,?,?,?,?)");
 		int	nextvalue1=GetCon.getPrimaryKey();
 		System.out.println("next value is "+nextvalue1);
 		ps.setInt(1,nextvalue1);
-		ps.setString(2,username);
+		ps.setString(2,personalnumber);
 		ps.setString(3, fullname);
-		ps.setString(4,password);
-		ps.setString(5,repassword);
-		ps.setDouble(6,amount);
-		ps.setString(7,adderess);
-		ps.setDouble(8,phone);
-		ps.setString(9,email);
+		
+		//ps.setDouble(4,amount);
+		ps.setString(4,address);
+		ps.setDouble(5,phone);
+		ps.setString(6,email);
+		ps.setString(7,statu);
+		ps.setDouble(8,0);
 		
 		status=ps.executeUpdate();
 		return status;
